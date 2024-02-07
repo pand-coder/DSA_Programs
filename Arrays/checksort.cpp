e8e0a56e6382add15f58ae243a29a40ab8826a59
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2024-02-07 20:56:08 +0530

    Create checksort.cpp

---
+++ Arrays/checksort.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+bool checkarraysort(int arr[],int n)
+{
+    int i=0;
+    for(i=1;i<n;i++)
+    {
+        if(arr[i]<arr[i-1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    bool a=checkarraysort(arr,n);
+    if(a)
+    {
+        cout<<"Array is sorted in ascending order";
+    }
+    else
+    {
+        cout<<"Array is not sorted";
+    }
+    return 0;
+}
