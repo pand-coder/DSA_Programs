7f6a2f71720e7200b60479e5024c5a7cb2de3856
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2024-02-09 18:08:32 +0530

    Create Basics.java

---
+++ Bits/Basics.java
@@ -0,0 +1,14 @@
+
+
+//Bitwise Left shit operator - "<<"
+public class Main
+{
+	public static void main(String[] args) {
+	int x=3;
+	System.out.println(x<<1); //6
+	System.out.println(x<<2); //12
+	int y=4;
+	int z = (x<<y); //48
+	System.out.println(z);
+	}
+}
