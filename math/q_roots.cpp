2841cfeadcabde0678d7ade5e59da32d745f64fa
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2024-02-12 23:03:53 +0530

    Update q_roots.cpp

--- math/q_roots.cpp
+++ math/q_roots.cpp
@@ -1,1 +1,32 @@
-
+class Solution {
+  public:
+    vector<int> quadraticRoots(int a, int b, int c) {
+        vector<int>ans;
+        double r1,r2;
+        double d=b*b-4*a*c;
+        if(d<0)
+        return {-1};
+        if(d==0)
+        {
+            r1=-b/(2.0*a);
+            r2=r1;
+        }
+        else
+        {
+            r1=(-b+sqrt(d))/(2.0*a);
+            r2=(-b-sqrt(d))/(2.0*a);
+        }
+        if(r1>=r2)
+        {
+            ans.push_back(floor(r1));
+            ans.push_back(floor(r2));
+        }
+        else
+        {
+            ans.push_back(floor(r2));
+            ans.push_back(floor(r1));
+        }
+        return ans;
+        
+    }
+};
