59fcbf912409ba54bc99f0cdef1f060bd0b7e62f
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2024-02-12 18:35:06 +0530

    Create xor_1_to_n.java

---
+++ Bits/xor_1_to_n.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        int rem=n%4;
+        if(rem==0)
+        {
+            System.out.println(n);
+        }
+        else if(rem==1)
+        {
+          System.out.println(1);  
+        }
+        else if(rem==2)
+        {
+            System.out.println(n+1);
+        }
+        else
+        {
+          System.out.println(0);  
+        }
+        
+        }
+    }
