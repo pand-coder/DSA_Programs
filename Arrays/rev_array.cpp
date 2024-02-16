3de4aea51ca83f32add7dc808b0d8ee9de16eb46
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2024-02-16 19:05:13 +0530

    Update rev_array.cpp

--- Arrays/rev_array.cpp
+++ Arrays/rev_array.cpp
@@ -17,3 +17,38 @@ int main()
     
     return 0;
 }
+
+// Another Approach
+
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main()
+{
+     int n;
+    cin>>n;
+    int arr[n],temp[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int i=n-1;
+    int j=0;
+    while(i>=0)
+    {
+        temp[j]=arr[i];
+        j++;
+        i--;
+    }
+    for(int k=0;k<n;k++)
+    {
+        arr[k]=temp[k];
+    }
+cout<<"Array elements after reversing:"<<endl;
+     for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+return 0;
+}
