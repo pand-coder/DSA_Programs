fa7b84ce629ae445441a925de519eed55a6ab70a
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2024-02-16 23:53:58 +0530

    Create rev_elements.cpp

---
+++ Arrays/rev_elements.cpp
@@ -0,0 +1,26 @@
+//Two pointer approach
+#include <iostream>
+using namespace std;
+int main()
+{
+     int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int i=0;
+    int j=n-1;
+    while(i<j) // break condition -> 
+    {
+       swap(arr[i],arr[j]);
+       i++;
+       j--;        
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+return 0;
+}
