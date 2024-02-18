04a273ba7ca1334e2dcad68ea41f4667fc3899fa
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2024-02-18 23:44:09 +0530

    Create SS_Des.cpp

---
+++ Sorting_Algos/SS_Des.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        int max=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]>arr[max])
+            {
+                max=j;
+            }
+        }
+        if(max!=i)
+        {
+            swap(arr[i],arr[max]);
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i];
+    }
+    return 0;
+}
