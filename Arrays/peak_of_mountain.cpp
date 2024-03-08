bb8f59a6cd14e7edb14114a621bcd8bb1c7ed6ec
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2024-03-08 23:26:10 +0530

    Create peak_of_mountain.cpp

---
+++ Arrays/peak_of_mountain.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int left = 0, right = arr.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < arr[mid + 1]) {  //arr[mid]>arr[mid-1]//
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+};
