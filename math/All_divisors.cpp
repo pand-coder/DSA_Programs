8ca3924718e24c28f0226e7ac8c8b6bbeb9bab48
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2024-03-13 22:56:50 +0530

    Update All_divisors.cpp

--- math/All_divisors.cpp
+++ math/All_divisors.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 void printdivisors(int n)
 {
-    for(int i=1;i*i<=n;i++)
+    for(int i=1; i <= sqrt(n); i++)
     {
         if(n%i==0)
         {
            cout<<i<<endl; 
+           if (i != n/i) // Print the other divisor if it's different
+               cout << n / i << endl;
         }
     }
 }
+
 int main()
 {
     int n;
