31c498a3d2606be0198859be141a4b3e3b7ac5ec
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2024-08-29 23:48:16 +0530

    Create p1.cpp

---
+++ Patterns/p1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int j=0;j<5;j++)
+    {
+    for(int i=0;i<5;i++)
+    {
+    cout<<"*"<<" ";
+    }
+    cout<<endl;
+    }
+    return 0;
+}
